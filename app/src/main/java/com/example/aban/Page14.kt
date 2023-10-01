65e57fa8fb8fd469770a82d7088262cea23f8d5a
EmanALZahrani <emansaalzahrani@gmail.com>
2023-10-01 03:13:43 +0300

    kjjkjk

--- app/src/main/java/com/example/aban/Page14.kt
+++ app/src/main/java/com/example/aban/Page14.kt
@@ -1,9 +1,10 @@
 package com.example.aban
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class Page14 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page14)
--- app/src/main/java/com/example/aban/page10.kt
+++ app/src/main/java/com/example/aban/page10.kt
@@ -1,137 +1,112 @@
 package com.example.aban
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.media.MediaRecorder
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.example.aban.databinding.ActivityPage10Binding
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Random
+
 
 private lateinit var binding: ActivityPage10Binding
-const val REQUEST_CODE = 200
+
 class page10 : AppCompatActivity() {
-
-    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
-    private var permissionGranted = false
-
-    private lateinit var recorder: MediaRecorder
-    private var dirPath=""
-    private var filename=""
-    private var isRecording=false
-    private var isPaused=false
-    @RequiresApi(Build.VERSION_CODES.S)
+    private lateinit var userChar: String // الحرف المختار من قبل المستخدم
+    private lateinit var textView: TextView // TextView لعرض الكلمة
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityPage10Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page10)
         setContentView(binding.root)
+
         next()
         back()
         acc()
 
-        permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
-        if (!permissionGranted)
-            ActivityCompat.requestPermissions(this,permissions, REQUEST_CODE)
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-         binding.apply {
-             btnRecord.setOnClickListener{
-                 when{
-                     isPaused->resumeRecorder()
-                     isRecording->pauseRecorder()
-                     else-> startRecording()
-                 }
-             }
-         }
+        // استعراض كلمات وحروف من Firebase Firestore
+        val wordsCollection = db.collection("words")
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+// Check if a user is signed in
+        if (currentUser != null) {
+            // Assuming your user documents are stored in a "users" collection
+            val db = FirebaseFirestore.getInstance()
+            val usersCollection = db.collection("users")
+
+            // Get the current user's document
+            val userId = currentUser.uid
+
+            usersCollection.document(userId).get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        // Retrieve the character field from the user's document
+                        val userChar = documentSnapshot.getString("selected_characters")
+
+                        if (userChar != null) {
+                            // Use the character value as needed
+                            // Now you have the current user's character
+                        } else {
+                            // Handle the case where "character" field is null
+                        }
+                    } else {
+                        // Handle the case where the user's document doesn't exist
+                    }
+                }
+                .addOnFailureListener {
+                    // Handle any errors that occur during the retrieval
+                }
+        } else {
+            // User is not signed in, handle accordingly
+        }
+
+
+
+        // قم بالبحث عن الحرف المطابق في مجموعة الكلمات والحروف
+        wordsCollection.document(userChar).get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val wordsList = documentSnapshot.get("words") as List<String>
+                    // عرض كلمة عشوائية في TextView
+                    val random = Random()
+                    val randomWord = wordsList[random.nextInt(wordsList.size)]
+                    textView.text = randomWord
+                } else {
+                    textView.text = "لا توجد كلمات متاحة لهذا الحرف."
+                }
+            }
+            .addOnFailureListener { exception ->
+                textView.text = "حدث خطأ: ${exception.message}"
+            }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == REQUEST_CODE)
-            permissionGranted=grantResults[0]== PackageManager.PERMISSION_GRANTED
-    }
 
-    private fun pauseRecorder(){
-        recorder.pause()
-        isPaused=true
+
+
+    private fun back() {
         binding.apply {
-            btnRecord.setImageResource(R.drawable.ic_recorde)
-        }
-    }
-
-    private fun resumeRecorder(){
-        recorder.resume()
-        isPaused=false
-        binding.apply {
-            btnRecord.setImageResource(R.drawable.ic_pause)
-        }
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun startRecording(){
-        if(!permissionGranted) {
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
-            return
-        }
-
-        //start record
-        dirPath = "${externalCacheDir?.absolutePath}/"
-        var simpleDateFormat = SimpleDateFormat("yyyy.MM.DD.hh.mm.ss")
-        var date = simpleDateFormat.format(Date())
-        filename = "audio_record_$date"
-        recorder= MediaRecorder(applicationContext).apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-            setOutputFile("$dirPath$filename.wav")
-
-            try {
-                prepare()
-            }catch (e: IOException){}
-
-            start()
-        }
-        binding.apply {
-            btnRecord.setImageResource(R.drawable.ic_pause)
-            isRecording=true
-            isPaused=false
-        }
-
-    }
-
-
-    private fun back(){
-        binding.apply {
-            back.setOnClickListener(){
-                startActivity(Intent(this@page10,page11::class.java))
+            back.setOnClickListener() {
+                startActivity(Intent(this@page10, page11::class.java))
             }
         }
     }
 
-    private fun next(){
+    private fun next() {
         binding.apply {
-            btnNext.setOnClickListener(){
-                startActivity(Intent(this@page10,LevelOne::class.java))
+            btnNext.setOnClickListener() {
+                startActivity(Intent(this@page10, LevelOne::class.java))
             }
         }
     }
 
-    private fun acc(){
+    private fun acc() {
         binding.apply {
-            account.setOnClickListener(){
-                startActivity(Intent(this@page10,page12::class.java))
+            account.setOnClickListener() {
+                startActivity(Intent(this@page10, page12::class.java))
             }
         }
     }
