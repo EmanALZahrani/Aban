8b4350b774caef2dcc4550cdd4f13f76b5fbdedd
EmanALZahrani <emansaalzahrani@gmail.com>
2023-10-05 05:26:31 +0300

    kjjkjk

--- app/src/main/java/com/example/aban/Checkletter.kt
+++ app/src/main/java/com/example/aban/Checkletter.kt
@@ -65,7 +65,7 @@ class Checkletter : AppCompatActivity() {
                                     ).show()
 
                                     // Start the Diagnosis activity
-                                    val intent = Intent(this@Checkletter, DashboardActivitycan::class.java)
+                                    val intent = Intent(this@Checkletter, DashboardActivity::class.java)
                                     startActivity(intent)
                                 }
                                 .addOnFailureListener { e ->
