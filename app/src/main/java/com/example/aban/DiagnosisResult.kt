74f3846d51422588c8e4a0fa0202a10a6755acc8
Shmael1 <alwafishmael1@gmail.com>
2023-10-07 02:01:07 +0300

    update

--- app/src/main/java/com/example/aban/DiagnosisResult.kt
+++ app/src/main/java/com/example/aban/DiagnosisResult.kt
@@ -3,6 +3,7 @@ package com.example.aban
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageView
@@ -10,40 +11,21 @@ import androidx.appcompat.widget.AppCompatTextView
 import com.airbnb.lottie.LottieAnimationView
 
 class DiagnosisResult : AppCompatActivity() {
+
     private lateinit var nextButton: AppCompatButton
-    private lateinit var playButton: AppCompatImageView
-    var pitchTv: AppCompatTextView? = null
-    var durationTv: AppCompatTextView? = null
-    var loudnessTv: AppCompatTextView? = null
-    var playClicked = true
-    private lateinit var lottieAnimationView: LottieAnimationView
+    private lateinit var typeIntent: String
+
+
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.diagnosis_result)
-        loudnessTv = findViewById(R.id.loudnessCard_Id)
-        durationTv = findViewById(R.id.durationCard_Id)
-        pitchTv = findViewById(R.id.pitchCard_Id)
-
-        gettingIntent()
+        typeIntent = intent.getStringExtra("typeIntent") ?: ""
 
 
-        //        Lottie animation ******************************************
-        lottieAnimationView = findViewById(R.id.lottie_animation_view_medium)
-        // Set animation file (assuming it's in the res/raw directory)
-        lottieAnimationView.setAnimation(R.raw.animation_wave)
-
-        // Optional: Set animation speed
-        lottieAnimationView.setSpeed(1f) // Default speed is 1x
-
-        // Optional: Set looping
-        lottieAnimationView.loop(true) // Default is false
-
-
-        // ***********************************************************************
-        nextButton = findViewById(R.id.nextButton)
+        nextButton = findViewById(R.id.back)
         nextButton.setOnClickListener(View.OnClickListener { v: View? ->
             startActivity(
                 Intent(
@@ -52,29 +34,7 @@ class DiagnosisResult : AppCompatActivity() {
                 )
             )
         })
-        playButton = findViewById(R.id.drawable)
-        playButton.setOnClickListener(View.OnClickListener { v: View? ->
-            if (playClicked) {
-                playClicked = false
-                lottieAnimationView.setVisibility(View.VISIBLE)
-                findViewById<View>(R.id.audioTitleTextView).visibility = View.GONE
-                // Start the animation
-                lottieAnimationView.playAnimation()
-            } else {
-                playClicked = true
-                lottieAnimationView.setVisibility(View.GONE)
-                findViewById<View>(R.id.audioTitleTextView).visibility = View.VISIBLE
-            }
-        })
+
     }
 
-    private fun gettingIntent() {
-        val recIntent = intent
-        val pitchRec = recIntent.getStringExtra("pitchIntent")
-        val durationRec = recIntent.getStringExtra("durationIntent")
-        val loudnessRec = recIntent.getStringExtra("loudnessIntent")
-        pitchTv!!.text = pitchRec
-        durationTv!!.text = durationRec
-        loudnessTv!!.text = loudnessRec
-    }
 }
