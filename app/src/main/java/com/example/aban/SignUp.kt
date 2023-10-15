a5d2788a0b82d0b2a0877525d300d793ca776dc5
Rana <ixrana.1421@gmail.com>
2023-10-16 00:14:03 +0300

    modify

--- app/src/main/java/com/example/aban/SignUp.kt
+++ app/src/main/java/com/example/aban/SignUp.kt
@@ -1,5 +1,4 @@
 package com.example.aban
-
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -50,83 +49,86 @@ class SignUp : AppCompatActivity() {
         SignUpButton.setOnClickListener {
             val intent = Intent(this, Checkletter::class.java)
             startActivity(intent)
-            // Handle user signup
-            handleUserSignup()
-        }
 
+
+        }
         // Set click listener for the "Sign In" button
         signinButton.setOnClickListener {
             // Navigate to the login page (replace LogIn::class.java with your actual login activity)
             val intent = Intent(this, LogIn::class.java)
             startActivity(intent)
         }
-    }
 
-    private fun handleUserSignup() {
-        val enteredUsername = username.text.toString()
-        val enteredPassword = password.text.toString()
-        val enteredPhoneNumber = phone.text.toString()
-        val enteredEmail = mail.text.toString()
+        // Set click listener for the sign-up button
+        SignUpButton.setOnClickListener {
+            val enteredUsername = username.text.toString()
+            val enteredPassword = password.text.toString()
+            val enteredPhoneNumber = phone.text.toString()
+            val enteredEmail = mail.text.toString()
 
-        if (isSignupValid(
-                enteredUsername,
-                enteredPassword,
-                enteredPhoneNumber,
-                enteredEmail,
-                confirmPassword.text.toString()
+            if (isSignupValid(
+                    enteredUsername,
+                    enteredPassword,
+                    enteredPhoneNumber,
+                    enteredEmail,
+                    confirmPassword.text.toString()
+                )
             )
-        ) {
-            // Create a new user with email and password using Firebase Authentication
-            auth.createUserWithEmailAndPassword(enteredEmail, enteredPassword)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // User account created successfully
-                        val user = auth.currentUser
-                        user?.let {
-                            val userId = it.uid
-                            val userDocRef = firestore.collection("users").document(userId)
 
-                            val userData = hashMapOf(
-                                "Email" to enteredEmail,
-                                "UserName" to enteredUsername,
-                                "PhoneNum" to enteredPhoneNumber,
-                                "password" to enteredPassword
+            // Handling user signup with Firestore data storage
 
 
-                            )
+                // Create a new user with email and password using Firebase Authentication
+                auth.createUserWithEmailAndPassword(enteredEmail, enteredPassword)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Save user data to Firestore
+                            val user = auth.currentUser
+                            user?.let {
+                                val userId = it.uid
+                                val userDocRef = firestore.collection("users").document(userId)
 
-                            userDocRef.set(userData)
-                                .addOnSuccessListener {
-                                    // User data stored successfully
-                                    // You can proceed to the next step or show a success message
-                                    // Save the user's email in SharedPreferences
-                                    val editor = sharedPreferences.edit()
-                                    editor.putString("email", enteredEmail)
-                                    editor.apply()
-                                    // Proceed to the next step or show a success message
-                                    Toast.makeText(
-                                        this@SignUp,
-                                        "Signup Successful!",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                                .addOnFailureListener { e ->
-                                    // Handle the error
-                                    Toast.makeText(
-                                        this@SignUp,
-                                        "Firestore Error: ${e.message}",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
+                                val userData = hashMapOf(
+                                    "Email" to enteredEmail,
+                                    "UserName" to enteredUsername,
+                                    "PhoneNum" to enteredPhoneNumber
+                                    // Add any other user data you want to store
+                                )
+
+                                userDocRef.set(userData)
+                                    .addOnSuccessListener {
+                                        // User data stored successfully
+                                        // You can proceed to the next step or show a success message
+                                        // Save the user's email in SharedPreferences
+                                        val editor = sharedPreferences.edit()
+                                        editor.putString("email", enteredEmail)
+                                        editor.apply()
+                                        // Proceed to the next step or show a success message
+                                        Toast.makeText(
+                                            this@SignUp,
+                                            "Signup Successful!",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                    .addOnFailureListener { e ->
+                                        // Handle the error
+                                        Toast.makeText(
+                                            this@SignUp,
+                                            "Firestore Error: ${e.message}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                            }
+                        } else {
+                            // Display an error message
+                            Toast.makeText(this, "Signup Failed! Please try again.", Toast.LENGTH_SHORT).show()
                         }
-                    } else {
-                        // Display an error message
-                        Toast.makeText(this, "Signup Failed! Please try again.", Toast.LENGTH_SHORT).show()
                     }
-                }
-        } else {
-            // Display an error message
-            Toast.makeText(this, "Signup Failed! Please check your inputs.", Toast.LENGTH_SHORT).show()
+            else {
+                // Display an error message
+                Toast.makeText(this, "Signup Failed! Please check your inputs.", Toast.LENGTH_SHORT).show()
+            }
+            }
         }
     }
 
@@ -138,13 +140,11 @@ class SignUp : AppCompatActivity() {
         confirmPassword: String
     ): Boolean {
         val isUsernameValid = username.isNotBlank() // Check if username is not empty
-        val isPasswordValid =
-            password.length >= 6 // Check if password is at least 6 characters long
+        val isPasswordValid = password.length >= 6 // Check if password is at least 6 characters long
         val isPhoneNumberValid =
             phoneNumber.matches(Regex("\\d+")) && phoneNumber.length == 10 // Check if the phone number contains 10 numeric digits
         val isEmailValid = isValidEmail(email) // Check if the email address has the correct format
-        val doPasswordsMatch =
-            password == confirmPassword // Check if the password and confirm password fields match
+        val doPasswordsMatch = password == confirmPassword // Check if the password and confirm password fields match
 
         return isUsernameValid && isPasswordValid && isPhoneNumberValid && isEmailValid && doPasswordsMatch
     }
@@ -153,4 +153,3 @@ class SignUp : AppCompatActivity() {
         val emailRegex = Regex("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
         return emailRegex.matches(email)
     }
-}
