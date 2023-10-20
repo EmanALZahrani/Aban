bdee3287240ce5b164ca8453de580e77ba28c572
MohraAlghamdi <mohrameme15@hotmail.com>
2023-10-20 17:12:10 +0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/aban/account.kt
+++ app/src/main/java/com/example/aban/account.kt
@@ -3,8 +3,12 @@ package com.example.aban
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -18,12 +22,12 @@ class account : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.account)
-        val button6 = findViewById<Button>(R.id.result)
-        button6.setOnClickListener {
+        // button to show result page
+        val button = findViewById<Button>(R.id.result)
+        button.setOnClickListener {
             val intent = Intent(this@account, DisplayAudioFilesActivity::class.java)
-            startActivity(intent)}
-
-
+            startActivity(intent)
+        }
 
         val user = auth.currentUser
         if (user != null) {
@@ -39,11 +43,124 @@ class account : AppCompatActivity() {
                         val username = document.getString("UserName") ?: "N/A"
                         val email = document.getString("Email") ?: "N/A"
                         val phoneNumber = document.getString("PhoneNum") ?: "N/A"
-
+                        val password = document.getString("password") ?: "N/A"
+                        val maskedPassword = maskPassword(password)
                         // Display data on TextViews
-                        findViewById<TextView>(R.id.nameresult).text = "$username"
-                        findViewById<TextView>(R.id.emailresult).text = "$email"
-                        findViewById<TextView>(R.id.phoneresult).text = "$phoneNumber"
+                        val nameTextView = findViewById<TextView>(R.id.nameresult)
+                        val emailTextView = findViewById<TextView>(R.id.emailresult)
+                        val phoneTextView = findViewById<TextView>(R.id.phoneresult)
+                        val passwordTextView = findViewById<TextView>(R.id.passwordresult)
+                        nameTextView.text = username
+                        emailTextView.text = email
+                        phoneTextView.text = phoneNumber
+                        passwordTextView.text = maskedPassword
+
+                        // Update data on button click
+                        val updateButton = findViewById<ImageButton>(R.id.update)
+                        updateButton.setOnClickListener {
+                            // Show EditText fields, OK button, and Cancel button
+                            findViewById<EditText>(R.id.nameEditText).visibility = View.VISIBLE
+                            findViewById<EditText>(R.id.emailEditText).visibility = View.VISIBLE
+                            findViewById<EditText>(R.id.phoneEditText).visibility = View.VISIBLE
+                            findViewById<EditText>(R.id.passwordEditText).visibility = View.VISIBLE
+                            findViewById<Button>(R.id.ok).visibility = View.VISIBLE
+                            findViewById<Button>(R.id.cancelButton).visibility = View.VISIBLE
+                            findViewById<Button>(R.id.result).visibility = View.GONE
+
+                            // Disable TextViews
+                            nameTextView.isEnabled = false
+                            emailTextView.isEnabled = false
+                            phoneTextView.isEnabled = false
+                            passwordTextView.isEnabled = false
+
+                            // Populate EditText fields with existing data
+                            findViewById<EditText>(R.id.nameEditText).setText(username)
+                            findViewById<EditText>(R.id.emailEditText).setText(email)
+                            findViewById<EditText>(R.id.phoneEditText).setText(phoneNumber)
+                            findViewById<EditText>(R.id.passwordEditText).setText(password)
+                        }
+
+                        // OK button onClickListener
+                        val okButton = findViewById<Button>(R.id.ok)
+                        okButton.setOnClickListener {
+                            // Get updated data from EditText fields
+                            val updatedName = findViewById<EditText>(R.id.nameEditText).text.toString()
+                            val updatedEmail = findViewById<EditText>(R.id.emailEditText).text.toString()
+                            val updatedPhoneNumber = findViewById<EditText>(R.id.phoneEditText).text.toString()
+                            val updatedPassword = findViewById<EditText>(R.id.passwordEditText).text.toString()
+
+                            // Create a map to hold the updated fields
+                            val updatedFields = mutableMapOf<String, Any>()
+                            // Check if the fields are updated and add them to the map
+                            if (updatedName != username) {
+                                updatedFields["UserName"] = updatedName
+                            }
+                            if (updatedEmail != email) {
+                                updatedFields["Email"] = updatedEmail
+                            }
+                            if (updatedPhoneNumber != phoneNumber) {
+                                updatedFields["PhoneNum"] = updatedPhoneNumber
+                            }
+                            if (updatedPassword != password) {
+                                updatedFields["password"] = updatedPassword
+                            }
+
+                            // Update the document in Firebase Database
+                            userDocRef.update(updatedFields)
+                                .addOnSuccessListener {
+                                    // Update successful
+                                    if (updatedFields.containsKey("UserName")) {
+                                        nameTextView.text = updatedFields["UserName"].toString()
+                                    }
+                                    if (updatedFields.containsKey("Email")) {
+                                        emailTextView.text = updatedFields["Email"].toString()
+                                    }
+                                    if (updatedFields.containsKey("PhoneNum")) {
+                                        phoneTextView.text = updatedFields["PhoneNum"].toString()
+                                    }
+                                    if (updatedFields.containsKey("password")) {
+                                        passwordTextView.text = updatedFields["password"].toString()
+                                    }
+                                }
+                                .addOnFailureListener { e ->
+                                    // Update failed
+                                    Toast.makeText(this@account, "Update failed", Toast.LENGTH_SHORT).show()
+                                }
+
+                            // Show TextViews
+                            nameTextView.isEnabled = true
+                            emailTextView.isEnabled = true
+                            phoneTextView.isEnabled = true
+                            passwordTextView.isEnabled = true
+                            findViewById<Button>(R.id.result).visibility = View.VISIBLE
+                            // Hide EditText fields, OK button, and Cancel button
+                            findViewById<EditText>(R.id.nameEditText).visibility = View.GONE
+                            findViewById<EditText>(R.id.emailEditText).visibility = View.GONE
+                            findViewById<EditText>(R.id.phoneEditText).visibility = View.GONE
+                            findViewById<EditText>(R.id.passwordEditText).visibility = View.GONE
+                            findViewById<Button>(R.id.ok).visibility = View.GONE
+                            findViewById<Button>(R.id.cancelButton).visibility = View.GONE
+
+                            // Show Update button again
+                            updateButton.visibility = View.VISIBLE
+                        }
+                        // Cancel button onClickListener
+                        val cancelButton = findViewById<Button>(R.id.cancelButton)
+                        cancelButton.setOnClickListener {
+                            // Show TextViews
+                            findViewById<TextView>(R.id.nameresult).visibility = View.VISIBLE
+                            findViewById<TextView>(R.id.emailresult).visibility = View.VISIBLE
+                            findViewById<TextView>(R.id.phoneresult).visibility = View.VISIBLE
+                            findViewById<TextView>(R.id.passwordresult).visibility = View.VISIBLE
+                            findViewById<Button>(R.id.result).visibility = View.VISIBLE
+                            // Hide EditText fields, OK button, and Cancel button
+                            findViewById<EditText>(R.id.nameEditText).visibility = View.GONE
+                            findViewById<EditText>(R.id.emailEditText).visibility = View.GONE
+                            findViewById<EditText>(R.id.phoneEditText).visibility = View.GONE
+                            findViewById<EditText>(R.id.passwordEditText).visibility = View.GONE
+                            findViewById<Button>(R.id.ok).visibility = View.GONE
+                            findViewById<Button>(R.id.cancelButton).visibility = View.GONE
+                        }
                     } else {
                         Log.d("Firestore", "No such document")
                     }
@@ -55,5 +172,14 @@ class account : AppCompatActivity() {
             // Handle the case when no user is signed in
             Log.d("FirebaseAuth", "No user is currently signed in.")
         }
+
+
     }
+
+    fun maskPassword(password: String): String {
+        return "*".repeat(password.length)
+    }
+
+
+    // Retrieve current user's ID
 }
