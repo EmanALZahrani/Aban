25a474a847a45214eadc62894e4cb9af9b6df2d6
ShmaelAlwafi <140494744+ShmaelAlwafi@users.noreply.github.com>
2023-10-20 18:52:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, request, jsonify
 import joblib
 import librosa
 import numpy as np
+from pydub import AudioSegment
 
-app = Flask(__name__)
+app = Flask(__name)
 
 @app.route('/')
 def index():
@@ -21,18 +22,21 @@ def contains_sound(audio, threshold=0.02):
     energy = np.sum(audio ** 2)
     return energy > threshold
 
-
-@app.route('/predict', methods=['POST','GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get the uploaded audio file from the request
         audio_file = request.files['audio']
-        audio, sample_rate = librosa.load(audio_file, sr=None)
+
+        # Convert the received AAC audio to WAV format
+        audio = AudioSegment.from_file(audio_file)
+        audio.export("input.wav", format="wav")
+
+        # Load the audio in WAV format for processing
+        audio, sample_rate = librosa.load("input.wav", sr=None)
 
         if not contains_sound(audio):
             return jsonify({"error": "The audio file is too silent. Please try again."})
-            
-        
 
         # Extract features from the audio
         features = features_extractor(audio, sample_rate)
@@ -43,14 +47,14 @@ def predict():
         # Make predictions using the Logistic Regression model
         probabilities = log_reg.predict_proba(features)
 
-# Assuming class 0 is "Normal" and class 1 is "Stutter"
+        # Assuming class 0 is "Normal" and class 1 is "Stutter"
         normal_prob = probabilities[0][0]
         stutter_prob = probabilities[0][1]
 
         return jsonify({"Normal": f"{normal_prob * 100:.2f}%", "Stutter": f"{stutter_prob * 100:.2f}%"})
 
     except Exception as e:
-        return jsonify({"error": "Something went wrong: " + str(e)})
+        return jsonify({"error": "Something went wrong: " + str(e})
 
-if __name__ == '__main__':
+if name == '__main__':
     app.run(debug=True)
