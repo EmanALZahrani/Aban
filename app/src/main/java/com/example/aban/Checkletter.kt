8d81e189f10c9d93c01fffd9766c138587750323
MohraAlghamdi <mohrameme15@hotmail.com>
2023-10-20 10:47:37 +0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/aban/Checkletter.kt
+++ app/src/main/java/com/example/aban/Checkletter.kt
@@ -1,9 +1,8 @@
 // Checkletter.kt
 package com.example.aban
 
-import android.content.Context
+
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.CheckBox
 import android.widget.Toast
@@ -13,9 +12,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 
+
 class Checkletter : AppCompatActivity() {
     private lateinit var binding: CheckletterBinding
-    private lateinit var sharedPreferences: SharedPreferences
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
 
@@ -24,13 +23,34 @@ class Checkletter : AppCompatActivity() {
         binding = CheckletterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Initialize SharedPreferences, Firebase Auth, and FirebaseFirestore
-        sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE)
+        // Initialize Firebase Auth and FirebaseFirestore
         auth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
 
-
-        next()
+        // Check if the user has already selected a letter
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            val userDocRef = firestore.collection("users").document(userId)
+            userDocRef.get()
+                .addOnSuccessListener { document ->
+                    if (document.exists() && document.getBoolean("letter_selected") == true) {
+                        // Start the Diagnosis activity directly
+                        val intent = Intent(this@Checkletter, Diagnosis::class.java)
+                        startActivity(intent)
+                        finish() // Finish the Checkletter activity to prevent going back
+                    } else {
+                        next()
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Handle the error
+                    Toast.makeText(
+                        this@Checkletter,
+                        "Firestore Error: ${e.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
     }
 
     private fun next() {
@@ -38,54 +58,45 @@ class Checkletter : AppCompatActivity() {
             next.setOnClickListener {
                 // Check if at least one character checkbox is checked
                 if (atLeastOneCharacterSelected()) {
-                    // Retrieve the user's email from SharedPreferences
-                    val userEmail = sharedPreferences.getString("email", "")
+                    // Retrieve the selected characters
+                    val selectedCharacters = getSelectedCharacters()
 
-                    if (!userEmail.isNullOrEmpty()) {
-                        // Retrieve the selected characters
-                        val selectedCharacters = getSelectedCharacters()
+                    // Store selected characters in Firestore
+                    val userId = auth.currentUser?.uid
+                    if (userId != null) {
+                        val userDocRef = firestore.collection("users").document(userId)
 
-                        // Store selected characters in Firestore
-                        val userId = auth.currentUser?.uid
-                        if (userId != null) {
-                            val userDocRef = firestore.collection("users").document(userId)
+                        val userSelections = hashMapOf(
+                            "selected_characters" to selectedCharacters,
+                            "letter_selected" to true
+                        )
 
-                            val userSelections = hashMapOf(
-                                "selected_characters" to selectedCharacters
-                            )
+                        userDocRef.set(userSelections, SetOptions.merge())
+                            .addOnSuccessListener {
+                                // User selections stored successfully in Firestore
+                                // Proceed to the next step or show a success message
+                                Toast.makeText(
+                                    this@Checkletter,
+                                    "Selections stored successfully.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
 
-                            userDocRef.set(userSelections, SetOptions.merge())
-                                .addOnSuccessListener {
-                                    // User selections stored successfully in Firestore
-                                    // Proceed to the next step or show a success message
-                                    Toast.makeText(
-                                        this@Checkletter,
-                                        "Selections stored successfully.",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-
-                                    // Start the Diagnosis activity
-                                    val intent = Intent(this@Checkletter, Diagnosis::class.java)
-                                    startActivity(intent)
-                                }
-                                .addOnFailureListener { e ->
-                                    // Handle the error
-                                    Toast.makeText(
-                                        this@Checkletter,
-                                        "Firestore Error: ${e.message}",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                        }
-                    } else {
-                        // Handle the case where the user's email is not available
-                        Toast.makeText(
-                            this@Checkletter,
-                            "User email not found. Please login again.",
-                            Toast.LENGTH_SHORT
-                        ).show()
+                                // Start the Diagnosis activity
+                                val intent = Intent(this@Checkletter, Diagnosis::class.java)
+                                startActivity(intent)
+                                finish() // Finish the Checkletter activity to prevent going back
+                            }
+                            .addOnFailureListener { e ->
+                                // Handle the error
+                                Toast.makeText(
+                                    this@Checkletter,
+                                    "Firestore Error: ${e.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                     }
                 } else {
+
                     // Display a message indicating that the user must select at least one character
                     Toast.makeText(
                         this@Checkletter,
@@ -138,4 +149,5 @@ class Checkletter : AppCompatActivity() {
         return selectedCharacters
     }
 }
+
 
