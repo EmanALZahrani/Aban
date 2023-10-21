0cb337e8a1d4450a7419513dfcd586029a465d6a
MohraAlghamdi <mohrameme15@hotmail.com>
2023-10-21 17:37:53 +0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/aban/Cancellation.kt
+++ app/src/main/java/com/example/aban/Cancellation.kt
@@ -67,7 +67,7 @@ class Cancellation : AppCompatActivity() {
         tvDuration = findViewById(R.id.tvDuration)
         lottieAnimationView = findViewById(R.id.lottie_animation_view)
         firestore = FirebaseFirestore.getInstance()
-       nextCans = findViewById(R.id.nextCans)
+       // nextCans = findViewById(R.id.nextCans)
 
         getRandomWord()
         val button6 = findViewById<ImageButton>(R.id.backBtn)
@@ -79,7 +79,8 @@ class Cancellation : AppCompatActivity() {
             val intent1 = Intent(this@Cancellation,account ::class.java)
             startActivity(intent1)}
         // Set click listener for the "nextCans" button
-        nextCans.setOnClickListener {
+        val next = findViewById<Button>(R.id.nextCans)
+        next.setOnClickListener {
             val intent3 = Intent(this@Cancellation, CancellationResult::class.java)
             startActivity(intent3)
         }
