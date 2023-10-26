93bf7071579274b5a47857a72232837c2b7c7b37
ShmaelAlwafi <140494744+ShmaelAlwafi@users.noreply.github.com>
2023-10-26 19:28:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the trained Logistic Regression model
-model_filename = 'Regression_Model11.pkl'
-scaler_filename = 'scaler11.pkl'
+model_filename = 'Regression_Model111.pkl'
+scaler_filename = 'scaler111.pkl'
 log_reg = joblib.load(model_filename)
 scaler = joblib.load(scaler_filename)
 
