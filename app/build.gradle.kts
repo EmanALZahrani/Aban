d4678e26ee0e26af153b926d3636fa7fab71f63c
Shmael1 <alwafishmael1@gmail.com>
2023-10-27 07:15:25 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.google.code.gson:gson:2.8.9")
 
     // Lottie animation
     implementation("com.airbnb.android:lottie:6.1.0")
